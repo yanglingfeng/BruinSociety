dfc093cb0eba51ba04ef4dc95884bf22991309d3
yanglingfeng <yanglingfeng@ucla.edu>
2016-11-22 02:52:47 -0800

    Improve showPost page

--- resources/views/showPost.blade.php
+++ resources/views/showPost.blade.php
@@ -3,12 +3,11 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-10 col-md-offset-1">
+            <div class="col-md-3">
                 <div class="panel panel-success">
                     <div class="panel-heading" style="font-weight: bold;
                     background-color: white; ">
-                            {{$post->title}}
-                        <a style="float: right" href="{{ url(action('DiscussionController@show', ['discussion_id'=>$discussion->id, 'society_id'=>$society->id]))}}">Back to Discussion Page</a>
+                        <a href="{{ url(action('DiscussionController@show', ['discussion_id'=>$discussion->id, 'society_id'=>$society->id]))}}">Back to Discussion Page</a>
                     </div>
 
                 @if(Auth::check())
@@ -16,23 +15,33 @@
                         <table class="table">
                             <tr>
                                 <th>Author</th>
-                                <th>Posted at</th>
-                                <th>Last Replied at</th>
                             </tr>
 
                             <tr>
                                 <td>{{$post->user_name}}</td>
+                            </tr>
+
+                            <tr>
+                                <th>Posted at</th>
+                            </tr>
+
+                            <tr>
                                 <td>{{$post->created_at}}</td>
+                            </tr>
+
+                            <tr>
+                                <th>Last Replied at</th>
+                            </tr>
+
+                            <tr>
                                 <td>{{$post->updated_at}}</td>
                             </tr>
 
                             @if($post->has_link==1)
                             <tr>
                                 <td>
-                                    <a href="{{url($file_url)}}">Click here to download the attached file</a>
+                                    <a href="{{url($file_url)}}">Download the attached file</a>
                                 </td>
-                                <td></td>
-                                <td></td>
                             </tr>
                             @endif
 
@@ -43,23 +52,19 @@
                     <a href="{{ url('/login') }}" class="btn btn-info"> You need to login to see the list 😜😜 >></a>
                 @endif
             </div>
-        </div>
-    </div>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-10 col-md-offset-1">
+            <div class="col-md-8">
                 <div class="panel panel-success">
-                    <div class="panel-heading" style="background-color: white; font-weight: bold;">Post Content</div>
+                    <div class="panel-heading" style="background-color: white; font-weight: bold;">{{$post->title}}</div>
 
-                @if(Auth::check())
+                    @if(Auth::check())
                         <p style="text-align: justify; margin: 10px">
                             @php
-                            $content = $post->content;
-                            echo nl2br($content);
+                                $content = $post->content;
+                                echo nl2br($content);
                             @endphp
                         </p>
-                @endif
+                    @endif
 
 
                 </div>
@@ -67,12 +72,8 @@
                     <a href="{{ url('/login') }}" class="btn btn-info"> You need to login to see the list 😜😜 >></a>
                 @endif
             </div>
-        </div>
-    </div>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-10 col-md-offset-1">
+            <div class="col-md-8">
                 <div class="panel panel-success">
                     <div class="panel-heading">Replies</div>
 
@@ -102,12 +103,8 @@
                     <a href="{{ url('/login') }}" class="btn btn-info"> You need to login to see the list 😜😜 >></a>
                 @endif
             </div>
-        </div>
-    </div>
-    <!-- Reply form -->
-    <div class="container">
-        <div class="row">
-            <div class="col-md-10 col-md-offset-1">
+
+            <div class="col-md-8">
                 <div class="panel panel-default">
                     <div class="panel-heading" style="font-weight: bold;">Reply</div>
                     <div class="panel-body">
@@ -115,10 +112,9 @@
                             {{ csrf_field() }}
 
                             <div class="form-group{{ $errors->has('content') ? ' has-error' : '' }}">
-                                <label for="content" class="col-md-4 control-label">Content</label>
 
-                                <div class="col-md-6">
-                                    <textarea id="content" type="text" class="form-control" name="content" value="{{ old('content') }}" required autofocus rows="5"> </textarea>
+                                <div class="col-md-12" >
+                                    <textarea  id="content" type="text" class="form-control" name="content" value="{{ old('content') }}" required autofocus rows="5"> </textarea>
 
                                     @if ($errors->has('content'))
                                         <span class="help-block">
@@ -133,7 +129,7 @@
                             <input type="hidden" name="post_id" value="{{$post->post_id}}" />
 
                             <div class="form-group">
-                                <div class="col-md-6 col-md-offset-4">
+                                <div class="col-md-6 col-md-offset-0">
                                     <button type="submit" class="btn btn-primary">
                                         Post
                                     </button>
@@ -145,5 +141,5 @@
             </div>
         </div>
     </div>
-    <!-- End of reply form -->
+
 @endsection
